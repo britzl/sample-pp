82dc1986ca057b31085ee2b1b2de8ea4db1f9089
Björn Ritzl <bjorn.ritzl@gmail.com>
2024-06-25 11:01:43 +0200

    Initial commit

---
+++ main/sheep.go
@@ -0,0 +1,43 @@
+components {
+  id: "entity"
+  component: "/main/pp/pp_entity.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"sheep_bounce\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/game.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 2.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 0.1
+    y: 0.1
+    z: 0.1
+  }
+}
